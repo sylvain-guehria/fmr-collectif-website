7d14d4645ec092005a1244c12340600bcff5c783
bloodysysy73 <sylvain.guehria@gmail.com>
2021-06-20 16:49:35 +0200

    feat(deploy): fix import

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { useRouter } from 'next/router';
-import db from '../../utils/db';
+import db from '../../api/firestore/index';
 
 const Post = (props) => {
   const { entry } = props;
