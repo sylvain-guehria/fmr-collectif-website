cb924e52a42639149b30437911cb39058ccf4674
bloodysysy73 <sylvain.guehria@gmail.com>
2021-06-30 08:51:28 +0200

    style():  style header + login button and add fmr logo

--- styles/jss/nextjs-material-kit-pro.js
+++ styles/jss/nextjs-material-kit-pro.js
@@ -309,7 +309,7 @@ const roseCardHeader = {
 const grayCardHeader = {
   color: whiteColor,
   background:
-    'linear-gradient(90deg, ' + grayColor[14] + ', ' + grayColor[14] + ')',
+    'linear-gradient(90deg, ' + grayColor[22] + ', ' + grayColor[21] + ')',
   ...grayBoxShadow
 };
 
