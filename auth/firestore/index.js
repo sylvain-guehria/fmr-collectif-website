1ac19533f35ae90fe0e8fe3892174f8dde532342
bloodysysy73 <sylvain.guehria@gmail.com>
2021-07-26 18:18:11 +0200

    feat(firestore): tsxfy pages

--- auth/firestore/index.js
+++ auth/firestore/index.js
@@ -2,6 +2,20 @@ import admin from 'firebase-admin';
 import serviceAccount from './serviceAccountKey.json';
 import logger from '../../modules/logger/logger';
 
+// const serviceAccount = {
+//   type: process.env.NEXT_PUBLIC_FIREBASE_TYPE,
+//   project_id:  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   private_key_id: process.env.NEXT_FIREBASE_PRIVATE_KEY_ID,
+//   private_key:process.env.NEXT_FIREBASE_PRIVATE_KEY,
+//   client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+//   client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+//   auth_uri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI,
+//   token_uri: process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_auth_provider_x509_cert_url,
+//   client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_client_x509_cert_url
+// };
+
+
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import firebaseUserRepository from '../../modules/user/firebaseUserRepository';
 import UserEntity from '../../modules/user/UserEntity';
-import { arrayOf } from 'prop-types';
 import UsersComponent from '../../components/Admin/users';
 import { buildAdminOnlyLayout } from '../../components/Layouts/layoutBuilder';
 
 const userRepository = new firebaseUserRepository();
 
-const UserPage = ({ users = [] })=> {
+interface Props {
+  users: UserEntity[];
+}
+interface staticProps {
+  props: Props;
+}
+
+const UserPage: React.FC<Props> = ({ users = [] }) => {
   return <UsersComponent users={users} />;
 };
 
-UserPage.propTypes = {
-  users: arrayOf(UserEntity)
-};
-
-export const getStaticProps = async () => {
+export const getStaticProps = async (): Promise<staticProps> => {
   const users = await userRepository.getAll();
   return { props: { users: JSON.parse(JSON.stringify(users)) } };
 };
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 UserPage.getLayout = buildAdminOnlyLayout();
 export default UserPage;
