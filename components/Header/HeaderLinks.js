d18937bf19e98989f98aa53d9dec7f8ea04bae45
bloodysysy73 <sylvain.guehria@gmail.com>
2021-10-07 19:14:04 +0200

    feat(refac): Refac HeaderLink, add headerItems and map it

--- components/Header/HeaderLinks.js
+++ components/Header/HeaderLinks.js
@@ -1,37 +1,14 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-key */
 import React from 'react';
 import { useAuth } from '../../hooks/useAuth';
 import { useNotification } from '../../hooks/useNotification';
-
+import headerItems from './headerItems';
 import PropTypes from 'prop-types';
-// react components for routing our app without refresh
 import Link from 'next/link';
-
-// @material-ui/core components
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-
-// @material-ui/icons
-import Apps from '@material-ui/icons/Apps';
-import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-import LineStyle from '@material-ui/icons/LineStyle';
-import AccountBalance from '@material-ui/icons/AccountBalance';
-import ArtTrack from '@material-ui/icons/ArtTrack';
-import Store from '@material-ui/icons/Store';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import PersonAdd from '@material-ui/icons/PersonAdd';
-import Call from '@material-ui/icons/Call';
-import Dns from '@material-ui/icons/Dns';
-
-import Build from '@material-ui/icons/Build';
-import People from '@material-ui/icons/People';
-
 import CustomDropdown from 'components/lib/CustomDropdown/CustomDropdown.js';
 import LoginButton from '../LoginButton/LoginButton';
-
 import styles from 'styles/jss/nextjs-material-kit-pro/components/headerLinksStyle.js';
 
 const useStyles = makeStyles(styles);
@@ -48,156 +25,37 @@ export default function HeaderLinks(props) {
     const summerReducer = (accumulator, curr) => accumulator + curr;
     return numberOfNotificationPerPages.reduce(summerReducer);
   };
-
   const { dropdownHoverColor } = props;
   const classes = useStyles();
   return (
     <List className={classes.list + ' ' + classes.mlAuto}>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="L'association"
-          buttonProps={{
-            className: classes.navLink,
-            color: 'transparent'
-          }}
-          buttonIcon={AccountBalance}
-          tabNotificationCounter={countTabNotification('association')}
-          dropdownList={[
-            <Link href="/about-us">
-              <a className={classes.dropdownLink}>
-                <ArtTrack className={classes.dropdownIcons} /> Qui somme nous
-              </a>
-            </Link>,
-            <Link href="/become-ambassador">
-              <a className={classes.dropdownLink}>
-                <PersonAdd className={classes.dropdownIcons} />
-                   Devenir membre
-                    </a>
-            </Link>,
-            <Link href="/contact-us">
-              <a className={classes.dropdownLink}>
-                <Call className={classes.dropdownIcons} />
-                  Nous contacter
-                   </a>
-            </Link>
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="Evènements"
-          buttonProps={{
-            className: classes.navLink,
-            color: 'transparent'
-          }}
-          buttonIcon={Apps}
-          dropdownList={[
-            <Link href="/blog-posts">
-              <a className={classes.dropdownLink}>
-                <ArtTrack className={classes.dropdownIcons} /> Le blog
-              </a>
-            </Link>,
-            <Link href="/events">
-              <a className={classes.dropdownLink}>
-                <Dns className={classes.dropdownIcons} /> Les évènements
-              </a>
-            </Link>
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="Boutique"
-          buttonProps={{
-            className: classes.navLink,
-            color: 'transparent'
-          }}
-          buttonIcon={Store}
-          dropdownList={[
-            <Link href="/ticket-office">
-              <a className={classes.dropdownLink}>
-                <LineStyle className={classes.dropdownIcons} />
-                 Billeterie
-              </a>
-            </Link>,
-            <Link href="/shop">
-              <a className={classes.dropdownLink}>
-                <ShoppingBasket className={classes.dropdownIcons} />
-                Shop
-              </a>
-            </Link>,
-            <Link href="/shopping-cart">
-              <a className={classes.dropdownLink}>
-                <ShoppingCart className={classes.dropdownIcons} />
-                Panier
-              </a>
-            </Link>
-          ]}
-        />
-      </ListItem>
-      {AuthUser && <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="Compte"
-          buttonProps={{
-            className: classes.navLink,
-            color: 'transparent'
-          }}
-          buttonIcon={AccountCircle}
-          dropdownList={[
-            <Link href="/profile">
-              <a className={classes.dropdownLink}>
-                <LineStyle className={classes.dropdownIcons} />
-                 Mon compte
-              </a>
-            </Link>,
-            <Link href="/orders">
-              <a className={classes.dropdownLink}>
-                <ShoppingBasket className={classes.dropdownIcons} />
-                Billets / achats
-              </a>
-            </Link>
-          ]}
-        />
-      </ListItem>}
-      {(AuthUser && AuthUser.isAdmin()) && <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          navDropdown
-          hoverColor={dropdownHoverColor}
-          buttonText="Admin"
-          buttonProps={{
-            className: classes.navLink,
-            color: 'transparent'
-          }}
-          buttonIcon={Build}
-          dropdownList={[
-            <Link href="/admin/users">
-              <a className={classes.dropdownLink}>
-                <People className={classes.dropdownIcons} />
-                 Utilisateurs
-              </a>
-            </Link>,
-            <Link href="/admin/stocks">
-              <a className={classes.dropdownLink}>
-                <People className={classes.dropdownIcons} />
-                Stocks
-             </a>
-            </Link>
-          ]}
-        />
-      </ListItem>}
+      {headerItems.map((item) => {
+        if (item.needsAuth && !AuthUser) return null;
+        if (item.adminOnly && !AuthUser.isAdmin()) return null;
+        return <ListItem className={classes.listItem} key={item.label}>
+          <CustomDropdown
+            noLiPadding
+            navDropdown
+            hoverColor={dropdownHoverColor}
+            buttonText={item.label}
+            buttonProps={{
+              className: classes.navLink,
+              color: 'transparent'
+            }}
+            buttonIcon={item.buttonIcon}
+            tabNotificationCounter={countTabNotification(item.notificationName)}
+            dropdownList={
+              item.pages.map((page) => {
+                return <Link href={page.href} key={page.label}>
+                  <a className={classes.dropdownLink}>
+                    <div className={classes.dropdownIcons} >{page.icon}</div>
+                    {page.label}
+                  </a>
+                </Link>;
+              })}
+          />
+        </ListItem>;
+      })}
       <ListItem className={classes.listItem}>
         <LoginButton />
       </ListItem>
---
+++ components/Header/headerItems.js
@@ -0,0 +1,118 @@
+import React from 'react';
+
+import Apps from '@material-ui/icons/Apps';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
+import LineStyle from '@material-ui/icons/LineStyle';
+import AccountBalance from '@material-ui/icons/AccountBalance';
+import ArtTrack from '@material-ui/icons/ArtTrack';
+import Store from '@material-ui/icons/Store';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import PersonAdd from '@material-ui/icons/PersonAdd';
+import Call from '@material-ui/icons/Call';
+import Dns from '@material-ui/icons/Dns';
+import Build from '@material-ui/icons/Build';
+import People from '@material-ui/icons/People';
+
+const headerItems = [
+  {
+    label: "L'association",
+    buttonIcon: AccountBalance,
+    notificationName: 'association',
+    pages: [
+      {
+        href: '/about-us',
+        label: 'Qui somme nous',
+        icon: <ArtTrack />
+      },
+      {
+        href: '/become-ambassador',
+        label: 'Devenir membre',
+        icon: <PersonAdd />
+      },
+      {
+        href: '/contact-us',
+        label: 'Nous contacter',
+        icon: <Call />
+      }
+    ]
+  },
+  {
+    label: 'Evènements',
+    buttonIcon: Apps,
+    notificationName: 'evenements',
+    pages: [
+      {
+        href: '/blog-posts',
+        label: 'Le blog',
+        icon: <ArtTrack />
+      },
+      {
+        href: '/events',
+        label: 'Les évènements',
+        icon: <Dns />
+      }
+    ]
+  },
+  {
+    label: 'Boutique',
+    buttonIcon: Store,
+    notificationName: 'boutique',
+    pages: [
+      {
+        href: '/ticket-office',
+        label: 'Billeterie',
+        icon: <LineStyle />
+      },
+      {
+        href: '/shop',
+        label: 'Shop',
+        icon: <ShoppingBasket />
+      },
+      {
+        href: '/shopping-cart',
+        label: 'Panier',
+        icon: <ShoppingCart />
+      }
+    ]
+  },
+  {
+    label: 'Compte',
+    needsAuth: true,
+    buttonIcon: AccountCircle,
+    notificationName: 'compte',
+    pages: [
+      {
+        href: '/profile',
+        label: 'Mon compte',
+        icon: <LineStyle />
+      },
+      {
+        href: '/orders',
+        label: 'Billets / achats',
+        icon: <ShoppingBasket />
+      }
+    ]
+  },
+  {
+    label: 'Admin',
+    needsAuth: true,
+    adminOnly: true,
+    buttonIcon: Build,
+    notificationName: 'admin',
+    pages: [
+      {
+        href: '/admin/users',
+        label: 'Utilisateurs',
+        icon: <People />
+      },
+      {
+        href: '/admin/stocks',
+        label: 'Stocks',
+        icon: <Dns />
+      }
+    ]
+  }
+];
+
+export default headerItems;
