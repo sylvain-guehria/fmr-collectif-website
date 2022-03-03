49dafe8496d2c329af81903a5142cfd7ea918eda
bloodysysy73 <sylvain.guehria@gmail.com>
2022-03-03 07:53:12 +0100

    feat(swr): remove SWR for now

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ToastProvider } from 'react-toast-notifications';
 import Axios from 'axios';
-import { SWRConfig } from 'swr';
 import LayoutComponent from '../components/Layouts/LayoutComponent';
 
 import App from 'next/app';
@@ -17,15 +16,6 @@ import '../listeners/routerListeners';
 
 Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
 Axios.defaults.withCredentials = true;
-
-const fetcher = async (url) => {
-  try {
-    const res = await Axios.get(url);
-    return res.data;
-  } catch (err) {
-    throw err.response.data;
-  }
-};
 
 export default class MyApp extends App {
 
@@ -44,32 +34,25 @@ export default class MyApp extends App {
 
     return (
       <React.Fragment>
-        <SWRConfig
-          value={{
-            fetcher,
-            dedupingInterval: 10000
-          }}
-        >
-          <ToastProvider>
-            <ProvideAuth>
-              <ProvideNotification>
-                <ProvideBoutique>
-                    <Head>
-                      <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                      />
-                      <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-                      <title>Fmr-collectif</title>
-                    </Head>
-                    <LayoutComponent component={Component} layoutProps={layoutProps}>
-                      <Component {...pageProps} />
-                    </LayoutComponent>
-                </ProvideBoutique>
-              </ProvideNotification>
-            </ProvideAuth>
-          </ToastProvider>
-        </SWRConfig>
+        <ToastProvider>
+          <ProvideAuth>
+            <ProvideNotification>
+              <ProvideBoutique>
+                <Head>
+                  <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                  />
+                  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+                  <title>Fmr-collectif</title>
+                </Head>
+                <LayoutComponent component={Component} layoutProps={layoutProps}>
+                  <Component {...pageProps} />
+                </LayoutComponent>
+              </ProvideBoutique>
+            </ProvideNotification>
+          </ProvideAuth>
+        </ToastProvider>
       </React.Fragment >
     );
   }
